52e4dafb7212ee707a5c78acd3a84019b9c383b9
Serkan Öztürk <serkanoztrk237@gmail.com>
2024-07-24 15:33:33 +0300

    Add project files.

---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP3;
+
+Console.WriteLine("Hello, World!");
+
+
+IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+IKrediManager tasitKrediManager = new TasitKrediManager();
+IKrediManager konutKrediManager = new KonutKrediManager();
+
+ILoggerService databaseLoggerService = new DatabaseLoggerService();
+ILoggerService fileLoggerService = new DatabaseLoggerService();
+
+BasvuruManager basvuruManager = new BasvuruManager();
+
+List<ILoggerService> loggerServices = new List<ILoggerService>(){new DatabaseLoggerService(),new FileLoggerService()};
+
+basvuruManager.BasvuruYap(tasitKrediManager,loggerServices);
+
+List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager};
+
+//basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+
---
+++ Oop2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Oop2;
+
+Console.WriteLine("Hello, World!");
+
+
+CoorporateCustomer customer1 = new CoorporateCustomer();
+
+IndividualCustomer customer2 = new IndividualCustomer();
+
+Customer customer3 = new CoorporateCustomer();
+
+Customer customer4 = new IndividualCustomer();
+
+CustomerManager customerManager = new CustomerManager();
+
+customerManager.Add(customer1);
+customerManager.Add(customer2);
+customerManager.Add(customer3);
+customerManager.Add(customer4);
